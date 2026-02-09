5697d29f96e1929fcc198a28f35e3d5e7bcb3194
coreysuter <corey.suter@kalidykia.com>
2026-02-09 14:21:39 -0600

    Update renderServicesHome.js

--- js/pages/renderServicesHome.js
+++ js/pages/renderServicesHome.js
@@ -355,7 +355,7 @@ const header = `
 `;
 
 const top5Panel = `
-    <div class="top5ScaleWrap" style="transform:scale(.5);transform-origin:top left;width:200%;max-width:200%;">
+    <div class="top5ScaleWrap" style="transform:scale(.65);transform-origin:top left;width:154%;max-width:154%;">
       <div class="panel techPickPanel">
       <div class="phead" style="border-bottom:none;padding:12px">
         <div class="pickHdrRow" style="margin-bottom:10px">
